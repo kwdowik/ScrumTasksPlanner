fbba5b82426205fbf2d4f7fff476a000de3288f7
kwdowik <wdowikkacper@gmail.com>
2018-02-05 20:13:08 +0100

    Added file with constans

---
+++ src/constans/ActionTypes.js
@@ -0,0 +1,10 @@
+export const LOGIN = 'LOGIN';
+export const ADD_TASK = 'ADD_TASK';
+export const SAVED_EDITED_TASK = 'SAVED_EDITED_TASK';
+export const EDIT_TASK_PROPERTY = 'EDIT_TASK_PROPERTY';
+export const CHANGE_LOGIN = 'CHANGE_LOGIN';
+export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
+export const CHANGE_PROJECT = 'CHANGE_PROJECT';
+export const RECEIVE_TASKS = 'RECEIVE_TASKS';
+export const TASK_DETAILS = 'TASK_DETAILS';
+export const ROOT_CHANGED = 'ROOT_CHANGED';
