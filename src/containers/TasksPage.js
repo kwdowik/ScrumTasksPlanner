2834f556cd7e3011206eb0ead85c3db663205cdf
kwdowik <wdowikkacper@gmail.com>
2018-02-05 20:12:09 +0100

    Added TasksPage container

---
+++ src/containers/TasksPage.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { TaskList } from '../components/TaskList';
+import { taskDetails } from "../actions/index";
+
+
+const TasksPage = ({tasks, dispatch}) => {
+    return (
+        <View style={styles.container}>
+            <TaskList
+                onPress={task => dispatch(taskDetails(task))}
+                tasks={tasks}
+            />
+        </View>
+    );
+};
+
+TasksPage.propTypes = {
+    tasks: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        url: PropTypes.string.isRequired,
+        priority: PropTypes.string.isRequired,
+        createDate: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        state: PropTypes.string.isRequired
+    })).isRequired
+};
+
+const mapStateToProps = state => ({
+    tasks: state.tasks.getAllTasks
+});
+
+export default connect(mapStateToProps)(TasksPage);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+});
+
