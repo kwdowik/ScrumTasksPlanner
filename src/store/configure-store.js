504ecfdb232f7db6ff4e601e7c1aca74e33b5b86
kwdowik <wdowikkacper@gmail.com>
2018-02-05 20:14:08 +0100

    Added store file

---
+++ src/store/configure-store.js
@@ -0,0 +1,19 @@
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from '../reducers/index';
+import { createLogger } from 'redux-logger';
+import { getAllTasks } from '../actions';
+
+
+const middleware = [ thunkMiddleware ];
+if (process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger());
+}
+
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+
+export default configureStore = (initialState) => {
+    const store = createStoreWithMiddleware(rootReducer, initialState);
+    store.dispatch(getAllTasks());
+    return store;
+}
