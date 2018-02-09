51efec41961b9ed17fc5d44ac4b25a5d8c188718
kwdowik <wdowikkacper@gmail.com>
2018-02-09 12:18:40 +0100

    Updated ActionTypes file

--- src/constans/ActionTypes.js
+++ src/constans/ActionTypes.js
@@ -1,11 +1,8 @@
-export const LOGIN = 'LOGIN';
-export const ADD_TASK = 'ADD_TASK';
 export const SAVED_EDITED_TASK = 'SAVED_EDITED_TASK';
-export const REMOVED_TASK = 'REMOVED_TASK';
 export const EDIT_TASK_PROPERTY = 'EDIT_TASK_PROPERTY';
-export const CHANGE_LOGIN = 'CHANGE_LOGIN';
-export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
-export const CHANGE_PROJECT = 'CHANGE_PROJECT';
+export const EDIT_USER_PROPERTY = 'EDIT_USER_PROPERTY';
 export const RECEIVE_TASKS = 'RECEIVE_TASKS';
 export const TASK_DETAILS = 'TASK_DETAILS';
-export const ROOT_CHANGED = 'ROOT_CHANGED';
+export const INVALID_USER = 'INVALID_USER';
+export const SET_USER = 'SET_USER';
+export const USER_REGISTERED = 'USER_REGISTERED';
