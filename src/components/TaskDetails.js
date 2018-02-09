0ef740fd6765520544fab77baa9d40eab6783dfe
kwdowik <wdowikkacper@gmail.com>
2018-02-09 12:25:35 +0100

    Changed way of select state and priority of task to selecting from dropdown

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Picker } from 'react-native';
 import { FormInput, FormLabel, Button } from 'react-native-elements';
+import { Dropdown } from 'react-native-material-dropdown';
 
 export const TaskDetails = ({task, editable, onEdit, onSave, onDelete}) => {
     return(
@@ -11,18 +12,24 @@ export const TaskDetails = ({task, editable, onEdit, onSave, onDelete}) => {
                 value={task.name}
                 onChangeText={e => onEdit(e, 'name')}
             />
-            <FormLabel>Priority</FormLabel>
+            <FormLabel>Project</FormLabel>
             <FormInput
                 inputStyle={styles.formInput}
-                value={task.priority}
+                value={task.projectName}
+                onChangeText={e => onEdit(e, 'projectName')}
+            />
+            <Dropdown
+                label='Priority'
+                containerStyle={styles.dropdownStyle}
+                data={dropdownPriorityData()}
                 onChangeText={e => onEdit(e, 'priority')}
             />
-            <FormLabel>State</FormLabel>
-            <FormInput
-                inputStyle={styles.formInput}
-                value={task.state}
-                onChangeText={e => onEdit(e, 'state')}
-            />
+           <Dropdown
+               label='State'
+               containerStyle={styles.dropdownStyle}
+               data={dropdownStateData()}
+               onChangeText={e => onEdit(e, 'state')}
+           />
             <FormLabel>Create date</FormLabel>
             <FormInput
                 inputStyle={styles.formInput}
@@ -36,23 +43,57 @@ export const TaskDetails = ({task, editable, onEdit, onSave, onDelete}) => {
                 onChangeText={e => onEdit(e, 'url')}
             />
             <Button
-                disabled={!editable}
+                disabled={!editable || !requiredPropertiesAreFillIn(task)}
                 onPress={() => onSave(task)}
                 raised
                 icon={{name: 'save'}}
                 buttonStyle={styles.addButtonStyle}
                 title='Save' />
-            {task !== undefined &&
+            {task.id !== undefined &&
             <Button
                 onPress={() => onDelete(task)}
                 icon={{name: 'delete'}}
                 buttonStyle={styles.deleteButtonStyle}
                 title='Delete' />
             }
-
         </View>
     );
 };
+
+const dropdownPriorityData = () => {
+    return [
+        {
+            value: 'low'
+        },
+        {
+            value: 'medium'
+        },
+        {
+            value: 'high'
+        }
+        ];
+};
+
+const dropdownStateData = () => {
+    return [
+        {
+            value: 'new'
+        },
+        {
+            value: 'in progress'
+        },
+        {
+            value: 'done'
+        }
+    ];
+};
+
+const requiredPropertiesAreFillIn = task => (
+        task.name !== undefined &&
+        task.projectName !== undefined &&
+        task.priority !== undefined &&
+        task.state !== undefined
+);
 
 const styles = StyleSheet.create({
     formInput: {
@@ -65,5 +106,9 @@ const styles = StyleSheet.create({
     deleteButtonStyle: {
         marginTop: 20,
         backgroundColor: '#df2020'
+    },
+    dropdownStyle: {
+        paddingLeft: 20,
+        paddingRight: 20,
     }
 });
