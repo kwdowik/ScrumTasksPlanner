e46bb68be9ad11c955e020eda8a548105e4037ce
kwdowik <wdowikkacper@gmail.com>
2018-02-11 21:26:11 +0100

    Added routing to server

---
+++ server/router.js
@@ -0,0 +1,29 @@
+import express, { Router } from 'express';
+// Import actions from controllers
+import { getAllUsers, createUser } from './controllers/user.controller';
+import {getAllTasks, createTask, updateTask, deleteTask} from "./controllers/task.controller";
+import {getAllProjects, createProject} from "./controllers/project.controller";
+
+// Initialize the router
+const router = Router();
+
+// Handle /users route with index action from movies controller
+router.route('/users')
+    .get(getAllUsers)
+    .post(createUser);
+
+// Handle /users route with index action from movies controller
+router.route('/tasks')
+    .get(getAllTasks)
+    .post(createTask);
+
+router.route('/tasks/:taskId')
+    .put(updateTask)
+    .delete(deleteTask);
+
+// Handle /users route with index action from movies controller
+router.route('/projects')
+    .get(getAllProjects)
+    .post(createProject);
+
+export default router;
